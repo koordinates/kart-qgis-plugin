91345078844c4c0a295e5ee531ff1f54eff2138d
volaya <volayaf@gmail.com>
2021-10-13 13:30:54 +0200

    cleaned unused code

--- kart/__init__.py
+++ kart/__init__.py
@@ -1,14 +1,3 @@
-# -*- coding: utf-8 -*-
-
-# (c) 2016 Boundless, http://boundlessgeo.com
-# This code is licensed under the GPL 2.0 license.
-
-import os
-import site
-
-site.addsitedir(os.path.abspath(os.path.dirname(__file__) + "/extlibs"))
-
-
 def classFactory(iface):
     from kart.plugin import KartPlugin
 
