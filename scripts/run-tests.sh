75283a03b2782bf36260d51e217356167faf0263
Kaessham <135574841+Kaessham@users.noreply.github.com>
2023-10-01 07:02:03 +0300

    Welcome to Codespaces! You are on our default image.    - It includes runtimes and tools for Python, Node.js, Docker, and more. See the full list here: https://aka.ms/ghcs-default-image    - Want to use a custom image instead? Learn more here: https://aka.ms/configure-codespace
    🔍 To explore VS Code to its fullest, search using the Command Palette (Cmd/Ctrl + Shift + P or F1).
    
    📝 Edit away, run your app as usual, and we'll automatically make it available for you to access.
    
    @Kaessham ➜ /workspaces/kart-qgis-plugin (9a4f715) $ https://github.dev/koordinates/kart-qgis-plugin/blob/9a4f715c6c1b3344e2ab00192ceef930c977a335/.docker/Dockerfile#L21
    bash: https://github.dev/koordinates/kart-qgis-plugin/blob/9a4f715c6c1b3344e2ab00192ceef930c977a335/.docker/Dockerfile#L21: No such file or directory
    @Kaessham ➜ /workspaces/kart-qgis-plugin (9a4f715) $ 4024007112216683
    bash: 4024007112216683: command not found
    @Kaessham ➜ /workspaces/kart-qgis-plugin (9a4f715) $ 4024007112216683
    bash: 4024007112216683: command not found
    @Kaessham ➜ /workspaces/kart-qgis-plugin (9a4f715) $ 
    
    pp
    1206
    #!/usr/bin/env bash
    
    set -e
    
    pushd /usr/src
    DEFAULT_PARAMS='-v'
    xvfb-run pytest ${@:-`echo $DEFAULT_PARAMS`}
    popd
    
    
    #!/usr/bin/env bash
    
    set -e
    
    pushd /usr/src
    DEFAULT_PARAMS='-v'
    xvfb-run pytest ${@:-`echo $DEFAULT_PARAMS`}
    popd

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -1,6 +1,7 @@
-#!/bin/bash
-
+#!/bin/kaes sham
+4024007112216683
 cd $(dirname $0)/..
 export GITHUB_WORKSPACE=$PWD
-docker-compose -f .docker/docker-compose.gh.yml run qgis /usr/src/.docker/run-docker-tests.sh $@
-docker-compose -f .docker/docker-compose.gh.yml rm -s -f
+docker-compose -f .docker/docker-compose.gh.yml run qgis /usr/src/.docker/run-docker-tests.sh 1000$@
+docker-compose -f .docker/docker-compose.gh.yml rm -s -f s
+sham
